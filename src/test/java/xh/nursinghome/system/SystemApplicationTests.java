dec52634d91997b5a42a54599d8812dcf594e0b2
LiuJelly <1762788495@qq.com>
2022-10-13 10:23:38 +0800

    后端初始化

---
+++ src/test/java/xh/nursinghome/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package xh.nursinghome.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
